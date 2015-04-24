ad95044e490a64a6ef191f8a3572611acb86da53
yamagao <yamagao@126.com>
2015-04-24 13:21:49 -0400

    add template

--- IndividualFullProfile.php
+++ IndividualFullProfile.php
@@ -1,8 +1,58 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html class="no-js" lang="en">
 <head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>IFAS EXPERTS Database</title>
+<!--Metadata-->
+<?php include 'include_meta.shtml'; ?>
+<!--End Metadata-->
+<!--Styles-->
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/override-main.css" />
+<link rel="stylesheet" href="css/single-menu-tabs.css" />
+<link rel='stylesheet' id='font-awesome-css'  href='css/font-awesome-4.2.0/css/font-awesome.min.css' type='text/css' media='all' />
+<!--Styles-->
+<!--Scripts-->
+<script src="js/vendor/modernizr.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+<!-- jQuery -->
+<script type="text/javascript" charset="utf8" src="jquery-1.10.2.min.js"></script>
+ <!--End Scripts-->
+ <!--Analytics-->
+<?php include 'include_analytics.shtml'; ?>
+<!--End Analytics-->
 </head>
 <body>
+<!--=================================Top Banner Area====================-->
+<!--UF/IFAS logo and site title-->
+<div class="row">
+  <div id="site-ID">
+    <div id="logo" class="medium-3 columns"><img src="css/images/UF-IFAS-logo.png" width="148" height="48" alt="Logo placeholder"></div>
+    <div id="site-title"  class="medium-9 columns">
+      <h1><a href="test.php">IFAS Communications</a></h1>
+    </div>
+  </div>
+</div>
+<!--END UF/IFAS logo and site title--> 
+<!--==============================Top Navigation========================================-->
+<!--Top navigation dropdown-->
+<div class="row">
+     <?php include 'include_top_nav_drop_down.shtml'; ?> 
+</div>
+<!--END top navigation--> 
+<!--Social media icons-->
+<div class="row">
+ <?php include 'include_social_media.shtml'; ?> 
+ </div>
+ <!--End Social Media icons-->
+<!--============================Main Content===============-->
+<!--============================Feature Boxes Area========================-->
+<div class="row">
+<div id="content" class="medium-12 columns">
+<!--Left column -->
+  <div id="content-left" class="medium-12 columns">
 <?php
 $expertID = $_GET["expert_id"];
 ?>
@@ -76,5 +126,34 @@ sqlsrv_close($connection);
 
 <input type="hidden" name="ExpertID" value=<?php echo $expertID;?> />
 <input type="submit" name="editExpert" value="Edit"/> 
+</div>
+    </div>
+      </div>
+<!--========================================Footer  and Footer Resources Area========================-->
+
+<!--Footer Resources-->
+<div id="footer-resources-wrapper" class="contain-to-grid">
+<div  class="row">
+   <?php include 'include_footer_resources.shtml'; ?> 
+</div></div>
+<!--END Footer Resources-->
+<!--Footer-->
+
+<div id="footer" class="contain-to-grid">
+<div class="row">
+  <?php include 'include_footer.shtml'; ?>
+  </div>
+  
+</div>
+<!--END Footer-->
+
+<!--Foundation Scripts-->
+<!-- <script src="js/vendor/jquery.js"></script> -->
+<script src="js/foundation.min.js"></script>
+ 
+<script>
+      $(document).foundation();
+    </script>
+    <!--END Foundation Scripts-->
 </body>
 </html>
