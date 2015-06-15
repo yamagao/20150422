1f7670a9247166e13a540b88f7ce608af010905b
yamagao <yamagao@126.com>
2015-06-15 16:53:36 -0400

    delete AOEID from another table also

--- UpdateAreaOfExpertise.php
+++ UpdateAreaOfExpertise.php
@@ -18,7 +18,7 @@ for($i = 1; $i <= 10; $i++)
 		continue;
 	}
 	
-	$insertAOE = "IF NOT EXISTS (SELECT * FROM AreaOfExpertise WHERE Name = " . str_replace("'","''",$_POST['addExpertise'.$i]) . ") INSERT INTO AreaOfExpertise (Name) VALUES ('" . str_replace("'","''",$_POST['addExpertise'.$i]) ."');";
+	$insertAOE = "IF NOT EXISTS (SELECT * FROM AreaOfExpertise WHERE Name = '" . str_replace("'","''",$_POST['addExpertise'.$i]) . "') INSERT INTO AreaOfExpertise (Name) VALUES ('" . str_replace("'","''",$_POST['addExpertise'.$i]) ."');";
 	
 	$aoeData = sqlsrv_query( $connection, $insertAOE);
 	
@@ -34,7 +34,16 @@ for($i = 1; $i <= 10; $i++)
 	if($_POST['deleteExpertise'.$i] == null){
 		continue;
 	}
-	$deleteAOE = "DELETE FROM AreaOfExpertise WHERE Name = " . str_replace("'","''",$_POST['deleteExpertise'.$i]) . ";";
+	$deleteAOE = "DELETE FROM AreaOfExpertise WHERE AreaOfExpertiseID = '" . str_replace("'","''",$_POST['deleteExpertise'.$i]) . "';";
+	
+	$expertiseData = sqlsrv_query( $connection, $deleteAOE);
+	
+	if( !$expertiseData ) {
+    	die( print_r( sqlsrv_errors(), true));
+	}
+	sqlsrv_commit($connection);	
+	
+	$deleteAOE = "DELETE FROM Expert_AreaOfExpertise WHERE AreaOfExpertiseID = '" . str_replace("'","''",$_POST['deleteExpertise'.$i]) . "';";
 	
 	$expertiseData = sqlsrv_query( $connection, $deleteAOE);
 	
@@ -46,7 +55,7 @@ for($i = 1; $i <= 10; $i++)
 
 sqlsrv_close($connection);
 
-header("Location: testyq.php");
+header("Location: EditAreaOfExpertise.php");
 ?>
 
 </body>
