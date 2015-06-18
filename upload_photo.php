c9ae45649af6701e310ea45931245942c30afc85
yamagao <yamagao@126.com>
2015-06-18 11:26:04 -0400

    add size limit 50k for thumbnail, 350k for large

--- upload_photo.php
+++ upload_photo.php
@@ -8,6 +8,12 @@ foreach ($_FILES["pictures"]["type"] as $key => $type){
 	//get the extension
 	$temp = explode(".", $_FILES["pictures"]["name"][$key]);
 	$extension = end($temp);
+	if($_POST["dir"] == "thumbnail/"){
+		$fileSize = 50;
+	}
+	if($_POST["dir"] == "large/"){
+		$fileSize = 350;
+	}
 	//check file type and extension
 	if ((($type == "image/gif")
 	|| ($type == "image/jpeg")
@@ -15,7 +21,7 @@ foreach ($_FILES["pictures"]["type"] as $key => $type){
 	|| ($type == "image/pjpeg")
 	|| ($type == "image/x-png")
 	|| ($type == "image/png"))
-	&& in_array($extension, $allowedExts)) {
+	&& in_array($extension, $allowedExts) && $_FILES["pictures"]["size"][$key] / 1024 < $fileSize) {
 		if ($_FILES["pictures"]["error"][$key] > 0) {
 			echo "Return Code: " . $_FILES["pictures"]["error"][$key] . "<br><br>";
 		} else {		
